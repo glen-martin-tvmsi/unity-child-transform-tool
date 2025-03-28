e5f2efcd583a3960d252388198296fde88beedf2
Glen+Adam <your-email@example.com>
2025-03-28 13:50:37 +0000

    Fix namespace

--- Assets/Editor/ChildPositionTool.cs
+++ Assets/Editor/ChildPositionTool.cs
@@ -1,20 +1,23 @@
 using UnityEditor;
 using UnityEngine;
 
-public class ChildPositionTool : Editor
+namespace LocalUnityChildTransformTool
 {
-    [MenuItem("GameObject/Set Children Transform", true, 20)]
-    static bool ValidateSetChildrenTransform()
+    public class ChildPositionTool : Editor
     {
-        return Selection.activeGameObject != null && 
-               Selection.activeGameObject.transform.childCount > 0;
-    }
+        [MenuItem("GameObject/Set Children Transform", true, 20)]
+        static bool ValidateSetChildrenTransform()
+        {
+            return Selection.activeGameObject != null && 
+                Selection.activeGameObject.transform.childCount > 0;
+        }
 
-    [MenuItem("GameObject/Set Children Transform", false, 20)]
-    static void InitTransformSetter(MenuCommand command)
-    {
-        var window = ScriptableObject.CreateInstance<ChildTransformWindow>();
-        window.parentTransform = Selection.activeGameObject.transform;
-        window.ShowModalUtility();
+        [MenuItem("GameObject/Set Children Transform", false, 20)]
+        static void InitTransformSetter(MenuCommand command)
+        {
+            var window = ScriptableObject.CreateInstance<ChildTransformWindow>();
+            window.parentTransform = Selection.activeGameObject.transform;
+            window.ShowModalUtility();
+        }
     }
 }
